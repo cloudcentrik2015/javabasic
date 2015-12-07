00d8a052b2d75650f15e77c09d4a70e7580953bb
info.cloudcentrik <info.cloudcentrik@gmail.com>
2015-12-07 18:18:48 +0100

    test commit

--- src/java4/hbs/client/Login.java
+++ src/java4/hbs/client/Login.java
@@ -1,4 +1,5 @@
 package java4.hbs.client;
+
 import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.image.*;
@@ -48,50 +49,47 @@ public class Login {
 		frame.setBounds(100, 100, 931, 580);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		textField = new JTextField();
 		textField.setBounds(507, 63, 349, 39);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(507, 170, 349, 39);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblUserName = new JLabel("User Name");
 		lblUserName.setBounds(349, 63, 147, 39);
 		frame.getContentPane().add(lblUserName);
-		
+
 		JLabel lblNewLabel = new JLabel("Password");
 		lblNewLabel.setBounds(349, 170, 132, 50);
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		JButton btnSignup = new JButton("SignUp");
-		btnSignup.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				Signup ab= new Signup();
+		btnSignup.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Signup ab = new Signup();
 				ab.signup();
-				
-			
-				
-			
 			}
 
-			
 		});
 		btnSignup.setBounds(507, 274, 164, 41);
 		frame.getContentPane().add(btnSignup);
-		
+
 		JButton btnSignin = new JButton("SignIn");
-		Image img = new ImageIcon(this.getClass().getResource("/ok.png")).getImage();
+		Image img = new ImageIcon(this.getClass().getResource("/ok.png"))
+				.getImage();
 		btnSignin.setIcon(new ImageIcon(img));
-		
+
 		btnSignin.setBounds(692, 274, 164, 41);
 		frame.getContentPane().add(btnSignin);
-		
+
 		JLabel label = new JLabel("");
-		Image img2 = new ImageIcon(this.getClass().getResource("/login1.png")).getImage();
+		Image img2 = new ImageIcon(this.getClass().getResource("/login1.png"))
+				.getImage();
 		label.setIcon(new ImageIcon(img2));
 		label.setBounds(15, 28, 320, 394);
 		frame.getContentPane().add(label);
