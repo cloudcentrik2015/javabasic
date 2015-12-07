4eff726706ceac6ff7f50275183ff7d1d3a4188e
java1-cloudcentrik <java1.cloudcentrik@gmail.com>
2015-12-07 18:42:28 +0100

    gitignore Test

---
+++ src/java1/TestDec07.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class TestDec07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is test 2");
+
+	}
+
+}
